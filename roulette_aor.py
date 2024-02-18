579bbeb2da29bf9e4e8885d7584e9686151fce54
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-02-17 23:42:40 -0500

    Add roulette_aor.py

---
+++ roulette_aor.py
@@ -0,0 +1,20 @@
+# Ahmed Roberts 
+# Roulette Tracking 3 Systems
+# 1. Martingale
+# 2. Parachute
+# 3. Ahmed's Can't Lose
+
+'''
+Roulette has 38 outcomes 1/38 = 
+
+http://www.fastodds.com/game-odds/roulette.htm
+Roulette, like craps, is an independent variable game where luck determines the winners. 
+An air of sophistication graces the table and the possibility of winning up to 35 times your bet 
+creates an enticing lure for many players. The odds in roulette are easy to calculate. 
+The wheel is divided into either 37 or 38 slots, depending on whether you are playing the European 
+version (single zero), or the American version which has an extra space in the double zero. 
+The zero space(s) represent the house edge. If there were none, it would be an even money game. 
+The house advantage in single zero roulette is 2.7% and for the double zero game it is 5.26%.
+
+
+'''
