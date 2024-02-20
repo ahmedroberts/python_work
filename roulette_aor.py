25e27c5834f722418cb90a12628895e5e5cca561
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-02-20 14:43:41 -0500

    Update roulette bet types

--- roulette_aor.py
+++ roulette_aor.py
@@ -18,3 +18,35 @@ The house advantage in single zero roulette is 2.7% and for the double zero game
 
 
 '''
+RED, BLACK = [], []
+ODD, EVEN = [], []
+FIRST12, SECOND12, THIRD12 = [], [], []
+FIRSTCOL, SECONDCOL, THIRDCOL = [], [], []
+ONETO18, NINETEENTO36 = [], []
+ZERO, DBLZERO = "0", "00"
+
+red_number = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+blk_number = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+odd_number = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+odd_number = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
+first_col  = [1,4,7,10,13,16,19,22,25,28,31,34]
+second_col = [2,5,8,11,14,17,20,23,26,29,32,35]
+third_col  = [3,6,9,12,15,18,21,24,27,30,33,36]
+
+'''
+for i in range(1, 37):
+    print(f"{i} modulo 3 = {i % 3}")
+'''
+
+for item in range(1, 37):
+    spin_msg = f"\t# {item} ~"
+    if item in red_number:
+        spin_msg += " red |"
+    else:
+        spin_msg += " black |"
+        
+    if item in odd_number:
+        spin_msg += " odd"
+    else:
+        spin_msg += " even"
+    print(spin_msg)
