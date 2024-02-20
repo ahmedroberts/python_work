0576dbc0df290bdf8d97c0015c4b220015b00935
Ahmed O. Roberts <aroberts@crossroadshealth.org>
2024-02-20 15:19:34 -0500

    Update for a color print a column check

--- roulette_aor.py
+++ roulette_aor.py
@@ -18,6 +18,41 @@ The house advantage in single zero roulette is 2.7% and for the double zero game
 
 
 '''
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
+
+
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+
+
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+
+
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk))
+
+
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
+
+
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
+
+
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk))
+
+
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk))
+
+'''
+Color Print Examples 
+https://www.studytonight.com/python-howtos/how-to-print-colored-text-in-python
+prCyan("Hello World, ")
+prYellow("It's")
+prGreen("Geeks")
+prRed("For")
+prGreen("Geeks")
+'''
+
+# Create Constants for bet
 RED, BLACK = [], []
 ODD, EVEN = [], []
 FIRST12, SECOND12, THIRD12 = [], [], []
@@ -26,27 +61,48 @@ ONETO18, NINETEENTO36 = [], []
 ZERO, DBLZERO = "0", "00"
 
 red_number = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-blk_number = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+black_number = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
 odd_number = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
 odd_number = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
 first_col  = [1,4,7,10,13,16,19,22,25,28,31,34]
 second_col = [2,5,8,11,14,17,20,23,26,29,32,35]
 third_col  = [3,6,9,12,15,18,21,24,27,30,33,36]
 
+RED = red_number
+print(f"Red Numbers: {RED}")
+
 '''
 for i in range(1, 37):
     print(f"{i} modulo 3 = {i % 3}")
 '''
 
-for item in range(1, 37):
+# Print Spin Results | Test
+prYellow("\n\n*******************\tSpin Results\t*******************\n")
+for item in range(1, 9):
+    # Red | Black
     spin_msg = f"\t# {item} ~"
     if item in red_number:
         spin_msg += " red |"
     else:
         spin_msg += " black |"
-        
+    
+    # Odd | Even
     if item in odd_number:
         spin_msg += " odd"
     else:
         spin_msg += " even"
-    print(spin_msg)
+        
+    # 1st, 2nd, or 3rd Column
+    if item in first_col:
+        spin_msg += " | column 1"
+    elif item in second_col:
+        spin_msg += " | column 2"
+    else:
+        spin_msg += " | column 3"
+        
+    # Display Results
+    if item in black_number:
+        prCyan(spin_msg)
+    else:
+        prRed(spin_msg)
+prGreen("\n*******************\t*************\t*******************\n\n")
