95c06a5927a8f3f13cd18fd50cde70fd48220db4
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-05 21:10:13 -0700

    :tada: create first dictionary of black supers

---
+++ practice_py/dictionary_001.py
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------
+# Ahmed Roberts The 9th Raikage 
+# June 2024 - Estrella Mountain CC
+# CIS156 Python Progamming - Level 1
+
+#-----------------------------------------------------------------------
+# Create my first ever python dictionary - 6/5/2024
+
+superhero_ids = {
+  'Blue Marvel':'Tchalla',
+  'Icon':'Augustus Freeman IV',
+  'Black Panther':"T'Challa, son of T'Chaka"
+  }
+
+for k, v in superhero_ids.items():
+  print(f'{k} is really {v}!😮')
+#-----------------------------------------------------------------------
