62001146969c58c0ce0d68f979e398caf69d9854
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-07 21:08:16 -0700

    :rocket: module 03 : types - practice

--- CIS_156/module_03__types.py
+++ CIS_156/module_03__types.py
@@ -3,9 +3,20 @@
 # CIS156 Python Progamming - Level 1
 
 #-----------------------------------------------------------------------
+# Strings
+greeting = 'Ahmed is the 9th Raikage!'
+passcode = '(th*R^1k^g#!'
+print(f'\"{greeting}\" is {len(greeting)} characters long.')
+print(f'\"{passcode}\" is {len(passcode)} characters long.')
 
 #-----------------------------------------------------------------------
-
+# Lists
+supers = ['Black Panther', 'Icon', 'Blue Marvel']
+print(supers)
+supers.append('Storm')
+supers.append('Aokiji')
+supers.append('Rocket')
+print(f'After appending:\n{supers}')
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
