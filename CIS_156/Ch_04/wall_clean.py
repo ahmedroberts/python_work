a515521464bd5ca0cce6442bd29c74bb7c328306
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-13 20:02:36 -0700

    :tada: wall_clean.py

---
+++ CIS_156/Ch_04/wall_clean.py
@@ -0,0 +1,19 @@
+# Ahmed O. Roberts | June 13, 2025
+# CIS 156 | Python Programming | Level 1
+# Module 04 Programming Assignment
+# Part A
+
+'''
+Wanda's Wall Cleaning offers regularly scheduled wall cleaning and maintenance services for homeowner's. Write a program called wall_clean.py to help customers choose a service plan. Prompt the user to input the following information:
+
+Wall height
+Number of cleaning visits per month
+Number of "deep cleaning" visits per year
+Based on the input, use branching to recommend appropriate service plan options:
+
+A customer with a wall height of 8 feet or less, with less than 4 visits per month and less than 3 deep cleanings per year should choose Plan A at $48 per month.
+A customer with a wall height of 8 feet or less, with 4 or more visits per month OR 3 or more deep cleanings per year should choose Plan B at $58 per month.
+A customer with a wall height of more than 8 feet, with less than 4 visits per month and less than 3 deep cleanings per year should choose Plan C at $62 per month.
+A customer with a wall height of more than 8 feet, with 4 or more visits per month OR 3 or more deep cleanings per year should choose Plan D at $66 per month.
+'''
+
