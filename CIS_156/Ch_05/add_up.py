195d707ba3df7b207690033f6b3bcb41e9720c90
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-18 09:02:12 -0700

    :rocket: Ch_05 assignment add_up.py :sparkles:

---
+++ CIS_156/Ch_05/add_up.py
@@ -0,0 +1,14 @@
+# Ahmed O. Roberts
+# Chapter 5 - Assignment
+
+# This program adds up a series of numbers
+
+sum = 0
+print('\nEnter the numbers to add, one at a time.\nEnter zero (\"0\") when finished.\n')
+num = int(input('Enter a number: '))
+
+while num != 0:
+  sum += num
+  num = int(input('Enter a number: '))
+
+print(f'\nThe sum of numbers you input is {sum}\n')
