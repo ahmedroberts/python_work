967bf95eb81b95ada6a5a1095536d4a033f805ef
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-22 22:24:52 -0700

    :rocket: make annual_costs.py ready for submission :sparkles:

--- CIS_156/Ch_06/annual_costs.py
+++ CIS_156/Ch_06/annual_costs.py
@@ -3,3 +3,21 @@
 # Part A
 
 # This program determines the cost of yearly vehicle ownership.
+
+print('\nFor the calculations enter monthly costs of the following items.\n')
+
+payment = float(input('Monthly cost of loan payment: '))
+insurance = float(input('Monthly cost of insurance premium: '))
+gas = float(input('Monthly cost of gas: '))
+oil_change = float(input('Monthly cost of oil changes: '))
+tires = float(input('Monthly cost of tires: '))
+maintenance = float(input('Monthly cost of general maintenance: '))
+
+def annual_cost(payment, insurance, gas, oil_change, tires, maintenance):  
+  costs = 0
+  costs = 12 * (payment + insurance + gas + oil_change + tires + maintenance)
+  return costs
+
+total_annual_cost = annual_cost(payment, insurance, gas, oil_change, tires, maintenance)
+
+print(f'\nToatal annual cost would be ${total_annual_cost:.2f}.\n')
