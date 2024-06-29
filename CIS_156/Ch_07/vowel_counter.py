4ede636b54285699d27db61df6d1bbb347fc064e
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-06-28 21:31:25 -0700

    :sparkles: working counter

--- CIS_156/Ch_07/vowel_counter.py
+++ CIS_156/Ch_07/vowel_counter.py
@@ -4,15 +4,7 @@
 
 # This program counts the number of vowels in a given string
 
-evaluated_string = "King Ahmed is the 9th Raikage  AEIOUY aeiouy!"
-
-# vowel_count = 0
-# count_a = 0
-# count_e = 0
-# count_i = 0
-# count_o = 0
-# count_u = 0
-# count_y = 0
+evaluated_string = "aaaaaaaaarrrgghhh! -- King Ahmed is the 9th Raikage  AEIOUY aeiouy!"
 
 vowel_dict = {
   'A': 'a',
@@ -26,34 +18,23 @@ vowel_dict = {
 def make_line():
   print(f'{"-" * 24}')
 
-make_line()  
-# for char in evaluated_string:
-#   if char == 'A' or char == 'a':
-#     count_a += 1
-#     print(char, count_a)
-  
-  # print(char)
-
-# vowel_count += count_a
-
-# print('A', count_a, '==> vc', vowel_count)
-make_line()  
-
-
-def count_vowels(my_dict):
+def count_vowels(my_dict, string_to_evaluate):
   vowel_count = 0
-  curr_vowel_count = 0
   for v in my_dict:
-    for char in evaluated_string:
+    curr_vowel_count = 0
+    for char in string_to_evaluate:
       if char == v or char == my_dict[v]:
         curr_vowel_count += 1
-        # print(v, my_dict[v], '==>', curr_vowel_count)
     print('---', v, char, curr_vowel_count)
-    curr_vowel_count = 0
-  vowel_count += curr_vowel_count
-  print('***', v, char, curr_vowel_count, vowel_count)
-  # print('vowel count ==>', vowel_count)
+    vowel_count += curr_vowel_count
+  print('*** Total Vowel Count: ', v, vowel_count)
+  print(char, curr_vowel_count, )
   
-count_vowels(vowel_dict)
+  return count_vowels
+
+print('\nEnter a phrase or sentence: ', evaluated_string)
+print('\nOccurrences of each vowel')
+make_line()   
+count_vowels(vowel_dict, evaluated_string)
 make_line()
  
