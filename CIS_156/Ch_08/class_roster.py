8389e035842dfd50c5e80814ca5f141d9747f67a
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-07-03 16:20:51 -0700

    :rocket: finalize class roster program

--- CIS_156/Ch_08/class_roster.py
+++ CIS_156/Ch_08/class_roster.py
@@ -3,22 +3,22 @@
 # Part A
 
 # This program allows the user to enter the students on a class roster 
-# after entering the number of students
+# - after entering the number of students
 
 roster = []
 greeting = '\nWelcome to the student roster creator\n'
-prompt_count = 'Enter the number of students: '
+prompt_count = 'Enter the number of math students: '
 prompt_student_entry = 'Enter student name: '
 
+# Greet
 print(greeting)
+# Get the number of students
 count_students = int(input(prompt_count))
-student_name = input(prompt_student_entry)
 
+# Add each student to roster from input
 for student in range(count_students):
-  print(student)
-  roster.append(student_name)
-  print(roster)
   student_name = input(prompt_student_entry)
-  print(roster)
+  roster.append(student_name)
   
-print('\n', roster)
+# Print the full roster
+print('\n', roster, '\n')
