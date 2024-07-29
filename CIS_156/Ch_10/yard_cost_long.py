7295b4feed6b98e8da09c9b70cbc476deb3e985a
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-07-29 15:17:53 -0700

    yard cost py finished

--- CIS_156/Ch_10/yard_cost_long.py
+++ CIS_156/Ch_10/yard_cost_long.py
@@ -8,15 +8,24 @@ try:
   lawn_length = float(input('\nEnter length in yards: '))
   lawn_width = float(input('Enter width of lawn: '))
   fertilizer_cost = float(input('Enter cost of fertilizer ($): '))
+  
+  if (lawn_length < 0):
+    raise ValueError('Lawn length must be a positve value')
+  if (lawn_width < 0):
+    raise ValueError('Lawn width must be a positve value')
+  if (fertilizer_cost < 0):
+    raise ValueError('Lawn length must be a positve value')
+  
+except ValueError as excpt:
+  print(f'Error: {excpt}')
 
   lawn_area = lawn_length * lawn_width
   cost_per_sq_yd = fertilizer_cost / lawn_area
-
-  print(f'\nArea of the lawn: {lawn_area} square yards.')
+  
+  print(f'Area of the lawn: {lawn_area} square yards.')
   print(f'Cost per square yard: ${cost_per_sq_yd:.2f} per square yard.\n')
-except ValueError:
-  print('\nPlease only enter numbers.\n')
+
 except ZeroDivisionError:
-  print('Lawn length nor width can equal zero.\n')
+  print('No values should equal zero.')
 except:
-  print('Sorry that did not work.\n\t-MGMT\n')
+  print('Sorry that did not work.\n\t-Raikage\n')
