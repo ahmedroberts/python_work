b7e6395ef381d1dc6c0a340178c74f0e919962e7
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-07-30 16:23:44 -0700

    :alembic: Programming Assignment Ch 12 Part A :mag:

---
+++ CIS_156/Ch_12/read_file.py
@@ -0,0 +1,30 @@
+# Ahmed O. Roberts
+# Module 12 Programming Assignment
+# Part A
+
+# This program returns a line from a file given the line number
+
+import os
+
+# Path of file on my system: gettysburg = os.path.join('CIS_156', 'Ch_12', 'address.txt')
+# Path of file: gettysburg = "address.txt"
+gettysburg = os.path.join("address.txt")
+
+# open the file
+myfile = open(gettysburg, 'r')
+lines = myfile.readlines()
+
+# Create a dynamic prompt
+print()
+prompt = f"Please enter a line number (1 - {len(lines)}) or 'q' to quit: "
+
+user_input = input(prompt)
+
+# get input until 'q' and display a line
+while user_input != 'q':
+  print(lines[int(user_input) - 1])
+  user_input = input(prompt)
+  
+# spacing and close
+print()
+myfile.close()
---
+++ CIS_156/Ch_12/test_it.py
@@ -0,0 +1,4 @@
+print('\nAhmed is the 9th Raikage!\n')
+
+prompt = "Please enter a line number or 'q' to quit"
+print(prompt)
