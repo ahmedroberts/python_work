2408750cd6892ce4befc4b5cc59c0ed42b584e4d
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-07-29 17:54:48 -0700

    :seedling: this submits module 11 :dizzy:

---
+++ CIS_156/Ch_11/math_module.py
@@ -0,0 +1,12 @@
+# Ahmed O. Roberts
+# Module 11 Programming Assignment
+# Part B
+
+# This program does a factorial an cubing operation on a given number
+
+import math
+
+our_num = int(input('\nEnter a number: '))
+
+print(f'{our_num} factorial is {math.factorial(our_num)}')
+print(f'{our_num} raised to the power of 3 is {math.pow(our_num, 3)}\n')
