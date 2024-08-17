c4c9ff66157779226819a2c03aa9f5f8966d2f6a
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-08-16 23:52:56 -0700

    :alien: add animal api data :green_heart:

--- API_practice/animals.py
+++ API_practice/animals.py
@@ -1,4 +1,10 @@
 import requests
+import json
+import os
+
+# Path of file on my system: gettysburg = os.path.join('CIS_156', 'Ch_12', 'address.txt')
+# Path of file if ran from same directory: gettysburg = "address.txt"
+gettysburg = os.path.join("address.txt")
 
 MY_API_KEY = 'qg+0oBakQjRBk7e7fBppSQ==94MKtUIGtIj3rlqh'
 
@@ -15,12 +21,21 @@ def animal_info(animal):
   api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal)
   response = requests.get(api_url, headers={'X-Api-Key': MY_API_KEY})
   if response.status_code == requests.codes.ok:
-      print(response.text)
+      # print(response.text)
+      animal_data = response.json()
   else:
       print("Error:", response.status_code, response.text)
+  return animal_data
       
-animals = ['panther', 'octopus', 'sloth', 'bear']
+# animals = ['panther', 'octopus', 'sloth', 'bear']
 # animal_info('octopus')
 
-for an in animals:
-  animal_info(an)
+# for beast in animals:
+#   animal_info(beast)
+
+animal_data = animal_info('sloth') 
+animal_data_path = os.path.join('API_practice', 'animal_data.json')
+with open(animal_data_path, 'w') as outfile:
+  json.dump(animal_data, outfile, ensure_ascii=False, indent=4)
+  
+print("\n\t-- All Done. --\n")
